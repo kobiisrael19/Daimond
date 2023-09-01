e124b00ab4bbbd63b14aba265cc2b595b09f227c
kobi_israeli <kobiisrael19@gmail.com>
2023-09-01 12:58:54 +0300

    ok

---
+++ src/components/hader/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='d-flex justify-content-center '>
+      <div >
+          <img style={{height:"120px",width:"500px"}} src="src/imgs/diamond logo.png" alt="logo" />
+      </div>
+    </div>
+  )
+}
+
+export default Logo
