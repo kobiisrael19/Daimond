6a9661760f6e77384070ef5ee55634f2b1374291
kobi_israeli <kobiisrael19@gmail.com>
2023-09-01 15:07:28 +0300

    ok

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 
-export const API_URL = "http://localhost:3004";
+export const API_URL = "https://tame-suspenders-mite.cyclic.app";
 
 export const TOKEN_KEY = "products_token";
 
